a6eedb25848079842bacd295861d60d0cd5f6a74
Mr Shaik Habeeb <shaikhabeeb0532@gmail.com>
2024-06-25 21:42:25 +0530

    added windows handle

---
+++ CodingPractice/src/test/java/com/selelniumpracetice/methods/TC_ClickButton.java
@@ -0,0 +1,59 @@
+package com.selelniumpracetice.methods;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TC_ClickButton {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.letskodeit.com/practice");
+		driver.findElement(By.id("bmwradio")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.id("benzradio")).click();
+		
+		driver.findElement(By.id("bmwcheck")).click();
+		driver.findElement(By.id("benzcheck")).click();
+		
+		driver.findElement(By.id("openwindow")).click();
+		
+		
+		// It will return the parent window name as a String
+		String parent=driver.getWindowHandle();
+
+		Set<String>s=driver.getWindowHandles();
+
+		// Now iterate using Iterator
+		Iterator<String> I1= s.iterator();
+
+		while(I1.hasNext())
+		{
+
+		String child_window=I1.next();
+
+
+		if(!parent.equals(child_window))
+		{
+		driver.switchTo().window(child_window);
+
+		System.out.println(driver.switchTo().window(child_window).getTitle());
+
+		driver.close();
+		}
+
+		}
+		//switch to the parent window
+		driver.switchTo().window(parent);
+
+	
+		
+		
+	}
+
+}
